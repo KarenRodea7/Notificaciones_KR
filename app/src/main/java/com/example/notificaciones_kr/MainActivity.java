79745f9ddfd07e4308dd562b617277cdc119cf71
Karitz <krnrodea@gmail.com>
2024-07-31 17:35:33 -0600

    Segundo commit

--- app/src/main/java/com/example/notificaciones_kr/MainActivity.java
+++ app/src/main/java/com/example/notificaciones_kr/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.notificaciones_kr;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +14,43 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button T, TP, N;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        T = findViewById(R.id.Toast);
+        TP = findViewById(R.id.ToastP);
+        N = findViewById(R.id.Not);
+
+        T.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Este es un Toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_LONG);
+
+                LayoutInflater inflater = getLayoutInflater();
+                View view = inflater.inflate(R.layout.toast_person, null);
+                toast.setView(view);
+                toast.show();
+            }
+        });
+
+        N.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
